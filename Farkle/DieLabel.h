3004b3dbfc7c9709e1ed384c4fc273c635df7415
Leandro Pessini <leandro@pessini.me>
2015-03-21 14:46:38 -0300

    Add a subclass called DieLabel

---
+++ Farkle/DieLabel.h
@@ -0,0 +1,13 @@
+//
+//  DieLabel.h
+//  Farkle
+//
+//  Created by Leandro Pessini on 3/21/15.
+//  Copyright (c) 2015 Brazuca Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DieLabel : UILabel
+
+@end
---
+++ Farkle/DieLabel.m
@@ -0,0 +1,21 @@
+//
+//  DieLabel.m
+//  Farkle
+//
+//  Created by Leandro Pessini on 3/21/15.
+//  Copyright (c) 2015 Brazuca Labs. All rights reserved.
+//
+
+#import "DieLabel.h"
+
+@implementation DieLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
